c2fbc36932e4039b529495b373cfc99c1fa32c12
Sagar Mittal <sagar@sagarmittal.com>
2024-06-13 23:01:16 -0700

    v2 firmware: add in display, temp, pwm calculation

--- firmware/generate_table.py
+++ firmware/generate_table.py
@@ -15,19 +15,20 @@ measurements = [
 ]
 
 R2 = 33e3
-ADC_MAX = 1023
+ADC_MAX = 1 << 12
+MAX_C = 100
 
 def celsius_from_adc(adc):
   return 213 - 34.3 * ln(adc)
 
 def adc_from_celsius(celsius):
-  return exp((213 - celsius) / 34.3)
+  return exp((213 - celsius) / 34.3) * (ADC_MAX / 1024)
 
 with open('adc_to_celsius.c', 'w') as f:
   print('#include "adc_to_celsius.h"', file=f)
-  print('const uint16_t celsius_from_adc_table[] = {', file=f)
-  print(', '.join('%d' % adc_from_celsius(c) for c in range(0, 100)), file=f)
+  print('const uint16_t adc_for_celsius[] = {', file=f)
+  print(', '.join('%d' % adc_from_celsius(c) for c in range(0, MAX_C)), file=f)
   print('};', file=f)
 with open('adc_to_celsius.h', 'w') as f:
   print('#include <stdint.h>', file=f)
-  print('extern const uint16_t celsius_from_adc_table[];', file=f)
+  print(f'extern const uint16_t adc_for_celsius[{MAX_C}];', file=f)
--- firmware/main.cc
+++ firmware/main.cc
@@ -1,11 +1,20 @@
 #include "main.h"
 
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <charconv>
+#include <cstdio>
+#include <functional>
+
 #include "adc.h"
+#include "adc_to_celsius.h"
 #include "gpio.h"
 #include "i2c.h"
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
 #include "stm32f1xx_hal.h"
 #include "tim.h"
-#include "adc_to_celsius.h"
 
 enum ADC_MODE {
   ADC_MODE_POLLING,
@@ -13,12 +22,58 @@ enum ADC_MODE {
   ADC_MODE_DMA,
 };
 
-static constexpr ADC_MODE kAdcMode = ADC_MODE_INTERRUPT;
+static constexpr ADC_MODE kAdcMode = ADC_MODE_POLLING;
+static constexpr bool PRIME = false;
 uint32_t adc_val;
-uint16_t tach1 = 0;
-uint16_t tach2 = 0;
+std::atomic<uint16_t> tach1 = 0;
+std::atomic<uint16_t> tach2 = 0;
 
-void set_pwm(uint32_t val) { TIM2->CCR4 = val; }
+void set_pwm_pct(uint32_t val) { TIM2->CCR4 = val; }
+
+int thermistor_adc_to_celsius(uint16_t adc) {
+  const auto &table = adc_for_celsius;
+  return std::distance(std::begin(table),
+                       std::lower_bound(std::begin(table), std::end(table), adc,
+                                        std::greater{}));
+}
+
+void ssd1306_WriteInt(uint16_t num, SSD1306_Font_t font, SSD1306_COLOR color) {
+  char str[10];
+  auto r = std::to_chars(std::begin(str), std::end(str), num);
+  *r.ptr = '\0';
+  ssd1306_WriteString(str, font, color);
+}
+
+uint8_t fan_speed(int temp) {
+  static constexpr uint8_t MIN_SPEED = 20;
+  static constexpr uint8_t MAX_SPEED = 100;
+  static constexpr uint8_t MAX_LINEAR_TEMP = 55;
+  static constexpr uint8_t MIN_LINEAR_TEMP = 40;
+  if constexpr (!PRIME) {
+    if (temp > MAX_LINEAR_TEMP) {
+      return MAX_SPEED;
+    } else if (temp > MIN_LINEAR_TEMP) {  // Linear range.
+      return (MAX_SPEED - MIN_SPEED) * (temp - MIN_LINEAR_TEMP) /
+                 (MAX_LINEAR_TEMP - MIN_LINEAR_TEMP) +
+             MIN_SPEED;
+    } else if (temp > 20) {  // 20C = 68F
+      return MIN_SPEED;
+    } else {
+      return MAX_SPEED;  // In case of a bad reading, run at full speed.
+    }
+  } else {
+    static uint8_t ticks = 0;
+    static constexpr uint8_t PERIOD = 64;
+    uint8_t qq = ticks++ % PERIOD;
+    if (qq < PERIOD / 4) {
+      return 255;
+    } else if (qq > 3 * PERIOD / 4) {
+      return 30;
+    } else {
+      return 255 - (qq - PERIOD / 4) * (255 - 30) / (PERIOD / 2);
+    }
+  }
+}
 
 int main(void) {
   HAL_Init();
@@ -27,6 +82,7 @@ int main(void) {
   MX_ADC2_Init();
   MX_I2C1_Init();
   MX_TIM2_Init();
+  ssd1306_Init();
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
   if constexpr (kAdcMode == ADC_MODE_POLLING) {
@@ -36,31 +92,56 @@ int main(void) {
   } else if constexpr (kAdcMode == ADC_MODE_DMA) {
     HAL_ADC_Start_DMA(&hadc2, &adc_val, 2);
   }
+  uint16_t i = 0;
   while (1) {
     tach1 = 0;
     tach2 = 0;
     HAL_Delay(1000);
-    auto t1 = tach1;
-    auto t2 = tach2;
+    uint16_t t1 = tach1;
+    uint16_t t2 = tach2;
+    // Convert to RPM, two pulses per revolution.
+    t1 = t1 * 60 / 2;
+    t2 = t2 * 60 / 2;
+    const int temp = thermistor_adc_to_celsius(adc_val);
+    set_pwm_pct(fan_speed(temp));
 
-    for (int i = 0; i < 10; i++) {
-      // HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-      HAL_Delay(250);
-      if constexpr (kAdcMode == ADC_MODE_POLLING) {
-        HAL_ADC_PollForConversion(&hadc2, 1);
-        HAL_ADC_ConvCpltCallback(&hadc2);
-      }
+    if constexpr (kAdcMode == ADC_MODE_POLLING) {
+      HAL_ADC_PollForConversion(&hadc2, 1);
+      HAL_ADC_ConvCpltCallback(&hadc2);
     }
+    ssd1306_Fill(Black);
+    ssd1306_SetCursor(0, 0);
+    ssd1306_WriteString("Tach1: ", Font_7x10, White);
+    ssd1306_WriteInt(t1, Font_7x10, White);
+    ssd1306_WriteString("rpm", Font_6x8, White);
+    ssd1306_SetCursor(0, 10);
+    ssd1306_WriteString("Tach2: ", Font_7x10, White);
+    ssd1306_WriteInt(t2, Font_7x10, White);
+    ssd1306_WriteString("rpm", Font_6x8, White);
+    ssd1306_SetCursor(0, 20);
+    ssd1306_WriteString("ADC: ", Font_7x10, White);
+    ssd1306_WriteInt(adc_val, Font_7x10, White);
+    ssd1306_SetCursor(0, 30);
+    ssd1306_WriteString("Temp: ", Font_7x10, White);
+    ssd1306_WriteInt(temp, Font_7x10, White);
+    ssd1306_WriteString("C", Font_7x10, White);
+    ssd1306_SetCursor(0, 40);
+    ssd1306_WriteString("PWM: ", Font_7x10, White);
+    ssd1306_WriteInt(TIM2->CCR4, Font_7x10, White);
+    ssd1306_WriteString("%", Font_7x10, White);
+    ssd1306_SetCursor(0, 50);
+    ssd1306_WriteString("i: ", Font_7x10, White);
+    ssd1306_WriteInt(i, Font_7x10, White);
+    ssd1306_UpdateScreen();
+
+    i++;
   }
   return 0;
 }
 
 extern "C" {
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
-  if (hadc == &hadc2) {
-    adc_val = HAL_ADC_GetValue(&hadc2);
-    TIM2->CCR4 = (adc_val * 100) >> 12;
-  }
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *) {
+  adc_val = hadc2.Instance->DR;
 }
 
 void ADC_IRQHandler(ADC_HandleTypeDef *hadc) { HAL_ADC_IRQHandler(hadc); }
@@ -68,10 +149,10 @@ void ADC_IRQHandler(ADC_HandleTypeDef *hadc) { HAL_ADC_IRQHandler(hadc); }
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
   switch (GPIO_Pin) {
     case TACH1_Pin:
-      tach1++;
+      tach1.fetch_add(1);
       break;
     case TACH2_Pin:
-      tach2++;
+      tach2.fetch_add(1);
       break;
   }
   HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
